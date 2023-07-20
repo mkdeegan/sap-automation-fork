fb7065d8134d92f355a08b4990f5a4138bf98748
Morgan Deegan <morgan.deegan@sapcontoso.com>
2023-07-20 14:11:49 -0700

    SBD

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-scs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-scs.tf
@@ -516,14 +516,10 @@ resource "azurerm_managed_disk" "cluster" {
   #             upper(var.application_tier.scs_os.os_type) == "WINDOWS" && var.application_tier.scs_high_availability ? 1 : 0
 
   count = (
-    local.enable_deployment &&
-    upper(var.application_tier.scs_os.os_type) == "WINDOWS" &&
-    var.application_tier.scs_high_availability
-    ) ? (
-    1
-    ) : (
-    0
-  )
+          local.enable_deployment &&
+          var.application_tier.scs_high_availability &&
+          upper(var.application_tier.scs_os.os_type) == "WINDOWS"
+          ) ? 1 : 0
 
 
 
