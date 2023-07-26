1b5d1bfa83b0f8c7e44b200fc87c8e7caaace294
Morgan Deegan <morgan.deegan@sapcontoso.com>
2023-07-26 09:39:36 -0700

    SBD

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/vm-hdb.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/vm-hdb.tf
@@ -409,13 +409,13 @@ resource "azurerm_managed_disk" "cluster" {
   count = (
             local.enable_deployment &&
             var.database.high_availability
-            # (
-            #   upper(var.database.os.os_type) == "WINDOWS" ||
+            (
+              upper(var.database.os.os_type) == "WINDOWS" || upper(var.database.os.os_type) == "LINUX"
             #   (
-            #     upper(var.database.os.os_type) == "LINUX"
+            #     upper(var.database.os.os_type) == "LINUX" &&
             # #     upper(var.application_tier.database_cluster_type) == "ASD"
             #   )
-            # )
+            )
           ) ? 1 : 0
   # count = (
   #           local.enable_deployment &&
